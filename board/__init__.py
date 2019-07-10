921e2c48b639b426c36acb2f72ae464035ece81f
gloomyline <1211071880@qq.com>
2019-07-10 13:18:29 +0800

    Add: instance folder to contain some secrets of app runtime cfgs.

--- board/__init__.py
+++ board/__init__.py
@@ -2,14 +2,14 @@
 # @Author:              AlanWang
 # @Date:                2019-07-09 18:01:30
 # @Last Modified by:    AlanWang
-# @Last Modified time:  2019-07-10 11:58:34
+# @Last Modified time:  2019-07-10 13:14:25
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_debugtoolbar import DebugToolbarExtension
 
-app = Flask('board')
+app = Flask('board', instance_relative_config=True)
 app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
