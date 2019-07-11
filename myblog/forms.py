b7e0fb26afddd85e1b35b6422355eb063708640f
Alan <wrf@xmfineart.cn>
2019-07-11 14:22:26 +0800

    Add: forms class. (#11)

---
+++ myblog/forms.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# @Author:              AlanWang
+# @Date:                2019-07-11 11:58:09
+# @Last Modified by:    AlanWang
+# @Last Modified time:  2019-07-11 14:19:08
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, HiddenField, validationError
+from wtforms.validators import DataRequired, Email, URL, Optional, Length
+from myblog.models import Category
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128)])
+    remember = BooleanField('Remember me.')
+    submit = SubmitField('Loginlll')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 60)])
+    category = SelectField('Category', coerce=int, default=1)
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.category.choices = [(category.id, category.name) for category in Category.query.order_by(category.name).all()]
+
+
+class CategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter(name=field.data).first():
+            raise validationError('Name already exsited.')
+
+
+class CommentForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
+    site = StringField('Site', validators=[Optional(), URL(), Length(0, 255)])
+    body = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class AdminForm(CommentForm):
+    author = HiddenField()
