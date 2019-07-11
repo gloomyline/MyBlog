a6d1cd1549216a262cb63549649c30913209447f
gloomyline <1211071880@qq.com>
2019-07-11 15:32:49 +0800

    Add: some blog endpoints.

--- myblog/views/blog.py
+++ myblog/views/blog.py
@@ -2,10 +2,8 @@
 # @Author:              AlanWang
 # @Date:                2019-07-10 14:33:08
 # @Last Modified by:    AlanWang
-# @Last Modified time:  2019-07-11 09:01:44
-import json
-import random
-from flask import Blueprint
+# @Last Modified time:  2019-07-11 14:33:04
+from flask import Blueprint, render_template
 from myblog.models import Category, Post, Comment
 
 
@@ -14,9 +12,19 @@ blog_bp = Blueprint('blog', __name__)
 
 @blog_bp.route('/')
 def index():
-    return '<p>Hello!</p>'
+    return render_template('views/index.html')
 
 
-@blog_bp.route('/posts')
-def fetch_posts():
-    return Post.query.get(random.randint(1, Post.query.count())).body
+@blog_bp.route('/about')
+def about():
+    return render_template('views/about.html')
+
+
+@blog_bp.route('/category/<int:category_id>')
+def show_category(category_id):
+    return render_template('blog/category.html')
+
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    return render_template('blog/post.html')
