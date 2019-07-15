ac9aacecd36f6a8825b7d9d6921ccab52eb48177
gloomyline <1211071880@qq.com>
2019-07-15 14:31:04 +0800

    Add: comments for post.

--- myblog/views/blog.py
+++ myblog/views/blog.py
@@ -2,7 +2,7 @@
 # @Author:              AlanWang
 # @Date:                2019-07-10 14:33:08
 # @Last Modified by:    AlanWang
-# @Last Modified time:  2019-07-15 11:23:54
+# @Last Modified time:  2019-07-15 14:21:56
 from flask import request, Blueprint, render_template, current_app
 from myblog.models import Category, Post, Comment
 
@@ -36,4 +36,17 @@ def show_category(category_id):
 @blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('blog/post.html', post=post)
+    per_page = current_app.config['MYBLOG_POST_PER_PAGE']
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.timestamp.desc()).paginate(page, per_page=per_page)
+    comments = pagination.items
+    return render_template('blog/post.html', post=post, comments=comments, pagination=pagination)
+
+
+@blog_bp.route('/reply/comment/<int:comment_id>')
+def reply_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if not comment.post.can_comment:
+        flash('Comment is disabled.', 'warning')
+        return redirect(url_for('.show_post', post_id=comment.post.id))
+    return redirect(url_for('.show_post', post_id=comment.post_id, reply=comment_id, author=comment.author) + '#comment-form')
