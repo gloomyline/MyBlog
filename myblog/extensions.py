dbe6bee7671a678b5380d5da6aab2e8c8222b861
Alan <wrf@xmfineart.cn>
2019-07-31 17:25:20 +0800

    Add: user auth.(#19)

--- myblog/extensions.py
+++ myblog/extensions.py
@@ -2,13 +2,14 @@
 # @Author:              AlanWang
 # @Date:                2019-07-10 14:44:38
 # @Last Modified by:    AlanWang
-# @Last Modified time:  2019-07-15 15:45:06
+# @Last Modified time:  2019-07-31 17:03:49
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_ckeditor import CKEditor
 from flask_moment import Moment
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,3 +17,16 @@ mail = Mail()
 moment = Moment()
 ckeditor =  CKEditor()
 login_manager = LoginManager()
+csrf = CSRFProtect()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from myblog.models import Admin
+    user = Admin.query.get(int(user_id))
+    return user
+
+
+login_manager.login_view = 'auth.login'
+login_manager.login_message = '请先登录'
+login_manager.login_message_category = 'warning'
--- myblog/views/auth.py
+++ myblog/views/auth.py
@@ -2,14 +2,35 @@
 # @Author:              AlanWang
 # @Date:                2019-07-10 14:32:59
 # @Last Modified by:    AlanWang
-# @Last Modified time:  2019-07-10 15:21:29
-from flask import Blueprint
+# @Last Modified time:  2019-07-31 11:45:25
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from myblog.models import Admin
+from myblog.forms import LoginForm
+from myblog.utils import redirect_back
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login')
 def login():
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for('blog.index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+        admin = Admin.query.first()
+        if admin:
+            if username == admin.username and admin.validate_password(password):
+                login_user(admin, remember)
+                flash('Welcome back.', 'info')
+                return redirect_back()
+            flash('Invalid username or password.', 'warning')
+        else:
+            flash('No account', 'warning')
+    return render_template('auth/login.html', form=form)
 
 
 @auth_bp.route('/logout')
